f299c39a269728e4370eabbeb19e9e656e6fe3ff
ZeroParadoxHome <77263046+ZeroParadoxHome@users.noreply.github.com>
2025-11-06 11:01:43 +0330

    Squashed 'Dart/' changes from edb66fd..10a4b1f
    10a4b1f Learned `for`, `for-in`, `forEach`, `while`, `do-while`, and labels like `continue`, `break` in loops with so many perfect examples
    
    git-subtree-dir: Dart
    git-subtree-split: 10a4b1f5c061603b88cb5263ce8e94cb484a2739

--- my_first_project/bin/control_flow.dart
+++ my_first_project/bin/control_flow.dart
@@ -218,4 +218,238 @@ void main() {
     _ => "A mismatch expression",
   };
   print(string7);
+  for (int i = 0; i < 3; i++) {
+    print(i);
+  }
+  for (int number12 = 1; number12 <= 10; number12++) {
+    if (number12 % 2 == 0) {
+      print(number12);
+    }
+    print(number12 % 2 == 0 ? number12 : "0");
+  }
+  int number13 = 1;
+  for (int number14 = 7; number14 >= 1; number14--) {
+    number13 *= number14;
+  }
+  print(number13);
+  int number15 = 1;
+  for (int number16 = 1; number16 <= 7; number16++) {
+    number15 *= number16;
+  }
+  print(number15);
+  var number17 = List.generate(7, (e) => e + 1).fold(1, (a, b) => a * b);
+  print(number17);
+  List list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (int i in list1) {
+    print(i);
+  }
+  int number18 = 0;
+  list1.forEach((i) => print("Position ${number18++} is assigned to $i"));
+  int number19 = 0;
+  while (number19 <= 10) {
+    print(number19);
+    number19++;
+  }
+  int number20 = 0;
+  while (number20 <= 25) {
+    if (number20 % 2 == 0) {
+      print(number20);
+    }
+    number20++;
+  }
+  int number21 = 7;
+  int number22 = 1;
+  while (number21 >= 1) {
+    number22 *= number21;
+    number21--;
+  }
+  print(number22);
+  int number23 = 1;
+  int number24 = 1;
+  while (number23 <= 7) {
+    number24 = number23 * number24;
+    number23++;
+  }
+  print(number24);
+  int number25 = 10;
+  do {
+    print(number25);
+    number25--;
+  } while (number25 >= 0);
+  int number26 = 15;
+  while (number26 >= 5) {
+    print(number26);
+    number26--;
+    if (number26 < 10) {
+      break;
+    }
+  }
+  for (int number27 = 0; number27 <= 10; number27++) {
+    print(number27);
+    if (number27 == 5) {
+      break;
+    }
+  }
+  for (int number28 = 1; number28 <= 5; number28++) {
+    if (number28 == 3) {
+      print("Now on 3");
+      continue;
+    }
+  }
+  int number27 = 5;
+  while (number27 <= 13) {
+    if (number27 % 2 == 0) {
+      number27++;
+      continue;
+    }
+    print(number27);
+    number27++;
+  }
+  outerLoop:
+  for (int x = 0; x <= 3; x++) {
+    for (int y = 0; y <= 3; y++) {
+      print("$x, $y");
+      if (x == 2 && y == 3) {
+        break outerLoop;
+      }
+    }
+  }
+  /* My own examples */
+  for (int number28 = 1; number28 <= 5; number28++) {
+    print(number28);
+  }
+  int number30 = 0;
+  for (int number29 = 1; number29 <= 10; number29++) {
+    number30 += number29;
+  }
+  print(number30);
+  for (int number38 = 0; number38 <= 20; number38++) {
+    if (number38 % 2 == 0) {
+      print(number38);
+    }
+  }
+  List<String> list2 = ["Zero", "Paradox", "Home"];
+  for (String n in list2) {
+    print(n);
+  }
+  int number32 = 0;
+  for (int number31 = 0; number31 <= 50; number31++) {
+    if (number31 % 3 == 0) {
+      number32 += number31;
+    }
+  }
+  print(number32);
+  List<double> list3 = [18.47, 16.93, 19.12, 14.58, 12.76];
+  List<double> list4 = [];
+  for (double g in list3) {
+    if (g > 15) {
+      list4.add(g);
+    }
+  }
+  print(list4);
+  List<double> list5 = list3.toSet().difference(list4.toSet()).toList();
+  print(list5);
+  List<double> list6 = list3
+      .where((element) => !list4.contains(element))
+      .toList();
+  print(list6);
+  List<double> list7 = [];
+  for (double s in list3) {
+    if (!list4.contains(s)) {
+      list7.add(s);
+    }
+  }
+  print(list7);
+  String string8 =
+      "Aute reprehenderit enim laboris sint pariatur veniam exercitation excepteur.";
+  int number33 = 0;
+  string8.toLowerCase();
+  for (int a = 0; a < string8.length; a++) {
+    if (string8[a] == "a") {
+      number33++;
+    }
+  }
+  print(number33);
+  int number34 = 0;
+  List<int> list8 = [12000, 45000, 8000, 23000, 15000];
+  for (int p in list8) {
+    number34 += p;
+  }
+  print(number34);
+  for (int number35 = 1; number35 <= 10; number35++) {
+    for (int number36 = 1; number36 <= 10; number36++) {
+      print(number35 * number36);
+    }
+  }
+  List<int> list9 = [2, 4, 5, 8, 11, 12, 17];
+  for (int f in list9) {
+    if ((f % f == 0) && (f % 1 == 0) && (f % 2 != 0)) {
+      print(f);
+    }
+  }
+  for (double z in list3) {
+    String string9 = switch (z) {
+      >= 18 => "Perfect",
+      >= 10 && < 18 => "Pass",
+      < 10 => "Failed",
+      _ => "An invalid grade",
+    };
+    print(string9);
+  }
+  for (double z in list3) {
+    if (z >= 18) {
+      print("Perfect");
+    } else if ((z >= 10) && (z < 18)) {
+      print("Pass");
+    } else {
+      print("Failed");
+    }
+  }
+  int number37 = 0;
+  while ((number37 <= 100) && (number37 >= 0)) {
+    if ((number37 % 3 == 0) && (number37 % 5 == 0)) {
+      print(number37);
+      number37++;
+    } else {
+      number37++;
+    }
+  }
+  list3.forEach((element) {
+    if (element case int n when n.isEven) {
+      print("$n is Even");
+    } else {
+      print("$element is Odd");
+    }
+  });
+  int number39 = 10;
+  while (number39 >= 0) {
+    if (number39 == 5) {
+      number39--;
+      continue;
+    }
+    print(number39);
+    number39--;
+  }
+  int number40 = 5;
+  do {
+    print(number40);
+    number40--;
+  } while (number40 == 5);
+  operationOuterLoop:
+  for (int number41 = 1; number41 <= 5; number41++) {
+    for (int number42 = 1; number42 <= 5; number42++) {
+      if (number41 * number42 == 12) {
+        print("now on 12");
+        break operationOuterLoop;
+      }
+    }
+  }
+  int number43 = 12345;
+  int number44 = 0;
+  String string10 = number43.toString();
+  for (int number45 = 0; number45 < string10.length; number45++) {
+    int number46 = int.parse(string10[number45]);
+    number44 += number46;
+  }
+  print(number44);
 }
