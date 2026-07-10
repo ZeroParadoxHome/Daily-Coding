ae8024edff7fd3a635438c0a745e923b6708a6e1
ZeroParadox <77263046+ZeroParadoxHome@users.noreply.github.com>
2026-07-10 13:20:04 +0330

    Squashed 'Flutter/' changes from 64fa731..f18c5bd
    f18c5bd Add SizedBox widget component and assorted_layout_widgets dependency
    
    git-subtree-dir: Flutter
    git-subtree-split: f18c5bd4921568fae12c8d2778a4da5ef4faff1a

---
+++ flutter_application_1/lib/pages/widgets/single_child_widgets/sizedbox.dart
@@ -0,0 +1,23 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:flutter/material.dart';
+
+class MySizedBox extends StatelessWidget {
+  const MySizedBox({super.key});
+
+  @override
+  Widget build(BuildContext context) => Material(
+    child: Column(
+      children: <Widget>[
+        SizedBox(
+          width: .infinity,
+          height: 15,
+          child: Container(width: 999, height: 999, color: Colors.blue),
+        ),
+        // const SizedBox(height: 1),
+        Box(height: 1, color: Colors.purple),
+        // Box.gap(.infinity),
+        // SizedBox.expand(child: Container(color: Colors.pink)),
+      ],
+    ),
+  );
+}
